25e48727c50297f4d629206ca8b977ef48854aa6
LonnonjamesD <LonnonjamesD@gmail.com>
2021-05-09 21:29:33 -0400

    e

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import tkinter as tk
-from PIL import Image, ImageTk
+import os
+
 
 class Application(tk.Frame):
 	def __init__(self, master=None):
@@ -8,14 +9,23 @@ class Application(tk.Frame):
 		self.master = master
 		self.pack()
 		self.create_widgets()
-		load = Image.open("page_01.png")
-		render = ImageTk.PhotoImage(load)
-		panel = tk.Label(root, image=img)
-		panel.pack(side="bottom", fill="both", expand="yes")
-		self.img = Label(root, image=render)
-		self.img.image = render
-		self.img.place(x=700/2.5, y=800/2.5)
+
 		self.iterate = 1
+
+		entries = os.listdir('Book/')
+		self.pagename = entries[0]
+		# Book/silent_sinner_prologue_01.jpg
+		# :-4
+		# -4
+		print(f'Book/{self.pagename[:-6]}{self.pagename[-4:]}')
+		self.image = PhotoImage(file=f'Book/{self.pagename[:-4]}')
+		self.label = Label(image=self.image)
+
+		self.pagetext = f"Page {self.iterate}"
+		self.pagenumber = Label(text=self.pagetext)
+
+		self.pagenumber.pack()
+		self.label.pack()
 		
 	def create_widgets(self):
 		self.nextpage = tk.Button(self)
@@ -27,20 +37,24 @@ class Application(tk.Frame):
 		self.lastpage.pack(side="top")
 		self.lastpage["command"] = self.last_page
 
+	def set_page(self):
+		try:
+			self.pagetext = f"Page {self.iterate}"
+			self.pagenumber.text = self.pagetext
+			self.image2 = PhotoImage(file=f"Book/page_0{self.iterate}.png")
+			print(f"page_0{self.iterate}.png")
+			self.label.configure(image=self.image2)
+			self.label.image = self.image2
+		except:
+			pass
+
 	def next_page(self):
-		load = Image.open(f"page_0{self.iterate}.png")
-		print(f"page_0{self.iterate}.png")
-		render = ImageTk.PhotoImage(load)
-		self.img.image = render
-		self.img.place(x=700/2.5, y=800/2.5)
+		self.iterate += 1
+		self.set_page()
 
 	def last_page(self):
-		load = Image.open(f"page_0{self.iterate}.png")
-		print(f"page_0{self.iterate}.png")
-		render = ImageTk.PhotoImage(load)
-		self.img.image = render
-		self.img.place(x=700/2.5, y=800/2.5)
-
+		self.iterate -= 1
+		self.set_page()
 
 
 root = tk.Tk()
